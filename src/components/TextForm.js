b08abfcb81567e638c336d6868018641c645160c
dikshaKapil <dikshakapil199@gmail.com>
2022-09-13 00:21:28 +0100

    12 sept commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const [text, setText] = useState('');
+    const [email, setEmailText] = useState('');
     // setText('new text');
     const handleUpClick=()=>{
         // console.log('upper case was clicked.'+text);
@@ -16,6 +17,15 @@ export default function TextForm(props) {
     const handleOnChange= (event)=>{
         // console.log('onChange');
         setText(event.target.value);
+    } 
+    const clearText=()=>{
+        setText('');
+        setEmailText('');
+    }
+    const extractEmail=()=>{
+        let newEmail = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+
+        setEmailText(newEmail);
     }
   return (
     <div>
@@ -23,14 +33,20 @@ export default function TextForm(props) {
         <h1>{props.heading}</h1>
         <div className="mb-3 ">
                 <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="10"></textarea>
+                <input type='label' className="form-control" id="myEmail" value={email} ></input>
+
                </div>
                <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
                <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
+               <button className="btn btn-primary mx-2" onClick={clearText}>Clear</button>
+               <button className="btn btn-primary mx-2" onClick={extractEmail}>Extract Email</button>
+
+
 
         </div>
         <div className="container my-3">
             <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} Words and {text.length} characters</p>
+            <p>{text.replace(/\n$/gm, '').split(/\n/).length} Paragraphs, {(text.split(" ").length-1)+(text.split(/\n/).length-1)} Words and {text.length} characters</p>
             <p>{0.08*text.split(" ").length} minutes to read.</p>
             <h3>Preview</h3>
             <p>{text}</p>
